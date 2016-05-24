63bc9368e6e1836c126c863bb205104ec937499a
Marc Troesken <m.troesken@mail.de>
2016-05-24 11:26:29 +0200

    Fix database files

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-var dbConfig = require('./knexfile.js')[process.env.NODE_ENV || 'development'];
+var dbConfig = require('../knexfile.js')[process.env.NODE_ENV || 'development'];
 
 var knex = require('knex')(dbConfig);
 var bookshelf = require('bookshelf')(knex);
